e0a96cb1f48f82f40c4f7608f71e80c20cdc8add
Neelesh <nt00605@gmail.com>
2020-01-11 16:17:28 +0530

    removing apikeys

--- routes/reset.js
+++ routes/reset.js
@@ -40,13 +40,13 @@ router.post('/forgot', function(req, res, next) {
       var smtpTransport = nodemailer.createTransport({
         service: 'Gmail', 
         auth: {
-          user: 'nt00605@gmail.com',
+          user: process.env.GMAILID,
           pass: process.env.GMAILPW
         }
       });
       var mailOptions = {
         to: user.email,
-        from: 'nt00605@gmail.com',
+        from: 'YelpCamp',
         subject: 'Node.js Password Reset',
         text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
           'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
